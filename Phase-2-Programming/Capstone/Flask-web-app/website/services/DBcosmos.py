7c5fbd3fd9308cde13015a993416c9b6558dc9f3
~Synpathy <Nicholas.Biz@protonmail.com>
2025-01-12 15:29:40 +1100

    Progress update

--- Phase-2-Programming/Capstone/Flask-web-app/website/services/DBcosmos.py
+++ Phase-2-Programming/Capstone/Flask-web-app/website/services/DBcosmos.py
@@ -1,8 +1,8 @@
 # Logic for querying/storing Pokémon data in Cosmos DB.
 from azure.cosmos import exceptions, CosmosClient, PartitionKey
-from website.services import secrets
+from .secrets import secrets
 
-client = CosmosClient(secrets.cosDB_Client_Auth, credential= secrets.cosDB_Client_Cred) # Make a .secrets file to store these 
+client = CosmosClient(secrets["cosDB_Client_Auth"], credential = secrets["cosDB_Client_Cred"]) # Make a .secrets file to store these 
 
 database = client.get_database_client("poke-db-id")
 container = database.get_container_client("poke-ct-id")
--- Phase-2-Programming/Capstone/Flask-web-app/website/services/blob_service.py
+++ Phase-2-Programming/Capstone/Flask-web-app/website/services/blob_service.py
@@ -4,5 +4,5 @@ from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 from website.services import secrets
 
 # Authenticat to Azure and access the Blob storage account. 
-connection_string = secrets.Blob_cnct_str # Make a .secrets file to store these 
+connection_string = secrets.get("Blob_cnct_str") # Make a .secrets file to store these 
 service = BlobServiceClient.from_connection_string(conn_str=connection_string) 
